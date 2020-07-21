58015fdf8bd641384e5f5c727d34f09c5e796aaf
Pier Francesco FAZZINI <fazzini@insa-toulouse.fr>
2020-07-21 16:41:38 +0200

    pyqt5ac generation process uses __init__.py

--- build_and_run.py
+++
@@ -1,6 +0,0 @@
-import pyqt5ac
-
-pyqt5ac.main(config='config.yml')
-
-from start_application import run
-run()
---
+++ widgets/__init__.py
@@ -0,0 +1,3 @@
+import pyqt5ac
+import os
+pyqt5ac.main(config=os.path.join(os.path.dirname(__file__), 'config.yml'))
